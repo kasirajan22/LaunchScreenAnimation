b428268bc27a1f9331de7f865f31dbea81aa0dd7
kasirajan22 <kasi.srm@gmail.com>
2020-10-03 21:02:21 +0530

    Initial Commit

---
+++ LunchScreenAnimation/LunchScreenAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchScreenAnimationApp.swift
+//  LunchScreenAnimation
+//
+//  Created by Magizhan on 03/10/20.
+//
+
+import SwiftUI
+
+@main
+struct LunchScreenAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
